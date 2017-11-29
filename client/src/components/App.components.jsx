aa6989452a37f55feccc34460d7fa04efe18ce42
john <ayalajohn82@gmail.com>
2017-11-29 10:25:01 -0800

    Add Jeff component

--- client/src/components/App.components.jsx
+++ client/src/components/App.components.jsx
@@ -5,25 +5,27 @@ import { connect } from 'react-redux';
 import Trip from '../components/Trip.component';
 import Dashboard from '../components/Dashboard.component';
 import LoginCont from '../containers/Login.container';
+import Jeff from '../components/Jeff';
 
 
 export class AppContainer extends React.Component {
 
   render() {
     console.log('this.props in app component', this.props);
-    if (this.props.isLoggedIn) {
+    // if (this.props.isLoggedIn) {
       return (
         <Router>
           <div>
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/trip" component={Trip} />
+            <Route path="/test" component={Jeff} />
           </div>
         </Router>
       );
-    }
-    return (
-      <LoginCont />
-    );
+    // }
+    // return (
+    //   <LoginCont />
+    // );
   }
 }
 
