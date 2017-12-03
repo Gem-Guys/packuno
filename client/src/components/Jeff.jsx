a8980cf56de62fb9140a30d2e00079be34353ee8
john <ayalajohn82@gmail.com>
2017-12-03 14:14:14 -0800

    rebase

--- client/src/components/Jeff.jsx
+++ client/src/components/Jeff.jsx
@@ -3,11 +3,8 @@ import AddTrip from '../containers/AddTrip.container';
 import Weather from '../containers/Weather.container';
 import TravelInformation from './TravelInformation.jsx';
 import CurrencyConverter from './CurrencyConverter.jsx';
-<<<<<<< f6a99207a7075030e3f10b954dad7afe271b08eb
 import CountryConditions from './CountryConditions.component';
-=======
 import Amazon from '../containers/Amazon.container';
->>>>>>> Add Amazon api call
 
 const addTripStyle = {
   display: 'inline-block',
@@ -26,11 +23,8 @@ const Jeff = () => (
       <TravelInformation />
     </div>
     <Weather />
-<<<<<<< f6a99207a7075030e3f10b954dad7afe271b08eb
     <CountryConditions />
-=======
     <Amazon />
->>>>>>> Add Amazon api call
   </div>
 );
 
